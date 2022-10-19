f26388bdd2c039edf8accef01e16fe29535f2d6b
Bilal <113285404+Bilal-Abykeev@users.noreply.github.com>
2022-10-19 14:16:54 +0300

    Add files via upload

---
+++ DZ2_BILAL_18-1/app.js
@@ -0,0 +1,38 @@
+const block2 = document.querySelector('.block');
+
+let position = 0
+let topPosition = 0
+
+
+
+const moveBlock = function () {
+    if(position <= 440 && topPosition == 0){
+        position+= 16
+        block2.style.left = `${position}px`
+        setTimeout(moveBlock, 100)
+    }
+    else if(position >= 440 && topPosition <= 440){
+        topPosition+= 16
+        block2.style.top = `${topPosition}px`
+        setTimeout(moveBlock, 100)
+    }
+    else if(position != 0 && topPosition >= 440){
+        position-= 16
+        block2.style.left = `${position}px`
+        setTimeout(moveBlock, 100)
+    }
+    else if(position == 0 && topPosition != 440){
+        topPosition-= 16
+        block2.style.top = `${topPosition}px`
+        setTimeout(moveBlock, 100)
+    }
+}
+moveBlock()
+
+
+let x = 0 
+const counter = () => {
+    x++
+    console.log(x)
+}
+setInterval(counter,1000)
